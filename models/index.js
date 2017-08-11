a82b6d3b929a34cc84bc6876ca256f9cd47d7724
Jonatas Saraiva <jonatas.saraiva@globalhitss.com.br>
2017-08-10 21:08:34 -0300

    Add model custom error

--- models/index.js
+++ models/index.js
@@ -1,10 +1,12 @@
 'use strict';
 const AppsModel = require('./apps'),
   AuthTokensModel = require('./auth-tokens'),
+  CustomError = require('./custom-error'),
   UsersModel = require('./users');
 
 module.exports = {
   AppsModel,
   AuthTokensModel,
+  CustomError,
   UsersModel
 };
