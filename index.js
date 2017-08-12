dbfbf1c422fe5aa35baad8426f57499c839039bc
Jonatas Saraiva <jonatas.saraiva@globalhitss.com.br>
2017-08-12 00:38:34 -0300

    Doc index project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 'use strict';
+//
+// require external internal
 const http = require('./http'),
   models = require('./models');
 
+//
+// grouped modules to export
 module.exports = {
   http,
   models
