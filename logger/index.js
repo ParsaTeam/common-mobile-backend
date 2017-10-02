577881ad8257c6d4e9f8a90cf5adf79e31eea1ad
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2017-10-01 21:40:09 -0300

    Change lib logger

--- logger/index.js
+++ logger/index.js
@@ -12,74 +12,48 @@ const WinstonCloudWatch = require('winston-cloudwatch');
 // date-separated stream.
 const startTime = new Date().toISOString();
 
-const LOG_ID = 'cloudwatch-api';
-winston.loggers.add(LOG_ID, {
-  transports: [
-    new WinstonCloudWatch({
-      logGroupName: `mobile-backend/${process.env.SERVICE_NAME}`,
-      logStreamName: () => {
-        // Spread log streams across dates as the server stays up
-        const date = new Date().toISOString().split('T')[0];
-        const cryptoMD5 = crypto.createHash('md5')
-          .update(startTime)
-          .digest('hex');
-        return `${date}-${cryptoMD5}`;
-      },
-      awsAccessKeyId: process.env.USER_CLOUDWACTH_ACCESS,
-      awsSecretKey: process.env.USER_CLOUDWACTH_SECRET,
-      awsRegion: process.env.USER_CLOUDWACTH_AWS_REGION,
-      awsOptions: {
-        logStreamName: process.env.USER_CLOUDWACTH_AWS_REGION
-      }
-    })
-  ]
+winston.add(WinstonCloudWatch, {
+  logGroupName: `mobile-backend/${process.env.SERVICE_NAME}`,
+  logStreamName: () => {
+    // Spread log streams across dates as the server stays up
+    const date = new Date().toISOString().split('T')[0];
+    const cryptoMD5 = crypto.createHash('md5')
+      .update(startTime)
+      .digest('hex');
+    return `${date}-${cryptoMD5}`;
+  },
+  awsAccessKeyId: process.env.USER_CLOUDWACTH_ACCESS,
+  awsSecretKey: process.env.USER_CLOUDWACTH_SECRET,
+  awsRegion: process.env.USER_CLOUDWACTH_AWS_REGION,
+  awsOptions: {
+    logStreamName: process.env.USER_CLOUDWACTH_AWS_REGION
+  },
+  jsonMessage: true
 });
 
 //
 // log methods to expose
 const log = {
-  info(message, category) {
-    const messageToLog = `${category} - ${message}`;
-    winston.loggers.get(LOG_ID).info(messageToLog);
+  info(message) {
+    winston.info(message);
   },
-  warn(message, category) {
-    const messageToLog = `${category} - ${message}`;
-    winston.loggers.get(LOG_ID).warn(messageToLog);
+  infoWithMetadata(message, metadata) {
+    winston.info(message, metadata);
   },
-  error(err, category, requestId) {
-    const messageToLog = {
-      category,
-      message: err.message,
-      stack: err.stack,
-      requestId
-    };
-
-    const message = `${category} - ${err.message}`;
-    winston.loggers.get(LOG_ID).error(message, messageToLog);
+  warn(message) {
+    winston.warn(message);
   },
-  warnCustonError(err, requestId) {
-    const messageToLog = {
-      category: err.category,
-      status: err.status,
-      message: err.message,
-      stack: err.orinalStack,
-      requestId
-    };
-
-    const message = `${err.category} - ${err.message}`;
-    winston.loggers.get(LOG_ID).warn(message, messageToLog);
+  warnWithMetadata(message, metadata) {
+    winston.warn(message, metadata);
   },
-  errorCustonError(err, requestId) {
-    const messageToLog = {
-      category: err.category,
-      status: err.status,
-      message: err.message,
-      stack: err.orinalStack,
-      requestId
-    };
-
-    const message = `${err.category} - ${err.message}`;
-    winston.loggers.get(LOG_ID).error(message, messageToLog);
+  error(err) {
+    const message = err.message;
+    delete err.message;
+    winston.error(message, err);
+  },
+  errorWithMetadata(err, metadata) {
+    metadata.stack = err.stack;
+    winston.error(err.message, metadata);
   }
 };
 
